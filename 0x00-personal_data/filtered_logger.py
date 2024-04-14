0ddf8a6aa5207b79a069d006a3f7a0ca27c14604
ngarukaalx <hiramkabuagi@gmail.com>
2024-04-14 07:30:34 +0000

    re.sub()

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,10 +7,7 @@ from typing import List
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """Returns the log msg obfuscated"""
-    msg_copy = message
     for field in fields:
-        pattern = r'(\b{}\s*=\s*)([^{}]*)(?=;)'.format(re.escape(field),
-                                                       re.escape(separator))
-        new_string = re.sub(pattern, r'\1' + redaction, msg_copy)
-        msg_copy = new_string
-    return msg_copy
+        message = re.sub(f"{field}=(.*?){separator}",
+                         f"{field}={redaction}{separator}", message)
+    return message
