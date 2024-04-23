7a39bfe5af3a8ea7b808bc22765b0faf5848f4e0
ngarukaalx <hiramkabuagi@gmail.com>
2024-04-23 21:22:28 +0000

    redister user_via_url

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """module constain app flask"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response, request
+from auth import Auth
+
+AUTH = Auth()
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
@@ -10,6 +13,21 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 def pay_load():
     """return JSON payload"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register_user():
+    """registers a user"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return make_response(
+                jsonify({"message": "email already registered"}),
+                400
+                )
 
 
 if __name__ == "__main__":
