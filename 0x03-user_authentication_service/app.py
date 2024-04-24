9a2d27bc8dedf92067fe287cd8dc61903fc30edf
ngarukaalx <hiramkabuagi@gmail.com>
2024-04-24 11:25:16 +0000

    login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """module constain app flask"""
-from flask import Flask, jsonify, make_response, request
+from flask import Flask, jsonify, make_response, request, abort
 from auth import Auth
 
 AUTH = Auth()
@@ -28,6 +28,24 @@ def register_user():
                 jsonify({"message": "email already registered"}),
                 400
                 )
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def log_in():
+    """login a registerd user"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    # check if credentials are fine
+    is_valid = AUTH.valid_login(email, password)
+    if is_valid:
+        session_id = AUTH.create_session(email)
+        response = make_response(
+                jsonify({"email": email, "message": "logged in"})
+                )
+        response.set_cookie("session_id", value=session_id)
+        return response
+    # abort if credentials are incorrect
+    abort(401)
 
 
 if __name__ == "__main__":
