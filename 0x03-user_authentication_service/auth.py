d0700a49a0a28b188be9b46eca316716e82bfa34
ngarukaalx <hiramkabuagi@gmail.com>
2024-04-24 19:06:50 +0000

    user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -71,7 +71,7 @@ class Auth:
         except InvalidRequestError:
             return
 
-    def get_user_from_session_id(self, session_id: str) -> User:
+    def get_user_from_session_id(self, session_id: str):
         """find user by session id"""
         if session_id is None:
             return None
