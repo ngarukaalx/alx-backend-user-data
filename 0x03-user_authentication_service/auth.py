5db424b5233bfb95cc9a8f417d1803d25005af41
ngarukaalx <hiramkabuagi@gmail.com>
2024-04-25 09:28:52 +0000

    update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -108,14 +108,14 @@ class Auth:
             # raise value error if the user does not exists
             raise ValueError()
 
-    def update_password(self, reset_token: str, password: str) -> None:
-        """update password"""
+    def update_password(self, reset_token: str, password: str):
+        """update password using reset_token"""
         try:
             user = self._db.find_user_by(reset_token=reset_token)
             hashed_pass = _hash_password(password)
             # update user with new password
             self._db.update_user(user.id, hashed_password=hashed_pass)
-            self._db.update_user(user.id, rest_token=None)
+            self._db.update_user(user.id, reset_token=None)
         except InvalidRequestError:
             raise ValueError()
         except NoResultFound:
